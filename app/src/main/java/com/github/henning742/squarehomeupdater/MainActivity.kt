d4343b1ba6038b02344e34bae81dd691b077ab95
henning724 <lynnharry_lhl@hotmail.com>
2020-10-16 20:30:22 +0800

    added folder picker

--- app/src/main/java/com/github/henning742/squarehomeupdater/MainActivity.kt
+++ app/src/main/java/com/github/henning742/squarehomeupdater/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.github.henning742.squarehomeupdater
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import lib.folderpicker.FolderPicker
+
+const val FOLDERPICKER_CODE = 1903
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,14 +20,27 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                    .setAction("Action", null).show()
+            val intent = Intent(this, FolderPicker::class.java)
+            startActivityForResult(intent, FOLDERPICKER_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == FOLDERPICKER_CODE && resultCode == Activity.RESULT_OK) {
+            val folderLocation = data!!.getStringExtra("data")
+//            Log.i("folderLocation", folderLocation)
+            Snackbar.make(findViewById(android.R.id.content), folderLocation.toString(), Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
+//        menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
